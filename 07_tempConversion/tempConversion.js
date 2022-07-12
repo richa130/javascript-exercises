f841b9f8c4c3f71970ee12d21d4316c2eeaf5178
richa130 <richabavadekar@me.com>
2022-07-12 11:22:08 -0700

    did ex07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(far) {
+  let cel = (far - 32) * (5/9);
 
+  if(cel % 1 != 0) { // checks if cel has a decimal or not
+    return +cel.toFixed(1);
+  }
+
+  return +cel; // unary plus converts a string to a number, like calling the Number() constructor
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+  let far = cel * (9/5) + 32
 
+  if(far % 1 != 0) {
+    return +far.toFixed(1);
+  }
+  
+  return +far;
 };
 
 // Do not edit below this line
