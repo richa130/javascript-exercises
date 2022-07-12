43f9da5d162e84cd96a11c13e3997752b72ff74f
richa130 <richabavadekar@me.com>
2022-07-12 10:33:14 -0700

    did ex03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    ret = "";
+    len = str.length;
 
+    for(let i = len - 1; i >= 0; i--) {
+        ret += str.charAt(i);
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
