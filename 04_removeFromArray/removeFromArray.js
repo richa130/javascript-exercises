02f1808fb1cc9a1f96dc5c37c1022e8d5c4dde0e
richa130 <richabavadekar@me.com>
2022-07-12 10:57:43 -0700

    Do ex04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, rem) {
+    const rems = Array.from(arguments);
 
+    let len1 = rems.length;
+    let len2 = arr.length;
+    let curr_rem;
+
+    for(let i = 1; i < len1; i++) { // we make it start from 1 because the first arg will be the array
+        curr_rem = rems[i];
+        
+        for(let j = 0; j < len2; j++) {
+            if(arr[j] ===  curr_rem) {
+                arr.splice(j, 1); // starting at element j, remove just one element w/out leaving any undefined holes
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
