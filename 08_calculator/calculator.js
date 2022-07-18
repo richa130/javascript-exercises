8ec497e9f34c3b82a7d7de00a34acac9f80d544e
richa130 <richabavadekar@me.com>
2022-07-18 11:02:08 -0700

    do ex08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  // reduce() takes a callback fn and an initial value
+  const running_sum = arr.reduce((x, y) => x + y, 0);
+  return running_sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((x, y) => x * y, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+
+// recursive fn
+const factorial = function(num) {
+  if(num == 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
