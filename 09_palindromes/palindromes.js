614a5826b02b4159b00ffd1502c2481685e9741d
richa130 <richabavadekar@me.com>
2022-07-18 11:55:55 -0700

    do ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    processedString = str.toLowerCase().replace(/[^a-z]/g, '');
+    return processedString.split('').reverse().join('') == processedString;
 };
 
 // Do not edit below this line
