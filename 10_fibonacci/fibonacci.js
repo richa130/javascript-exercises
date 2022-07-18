6cc8e2ceeb104a65980f8d2589376d55248c2f42
richa130 <richabavadekar@me.com>
2022-07-18 12:09:22 -0700

    Do ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
 
+    if(num  < 0) {
+        return 'OOPS';
+    }
+
+    if(num == 0) {
+        return 0;
+    }
+
+    if(num == 1 || num == 2 ) {
+        return 1;
+    }
+    
+    let a = 1;
+    let b = 1;
+    let sum = a + b;
+
+    for(let i = 3; i < num; i++) {
+        a = b;
+        b = sum;
+        sum = a + b;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
